4442333fe4ed155b62d91f37db0048efb5929bd4
kimmabrandt <kimmabrandt@gmail.com>
2016-10-26 08:46:52 -0700

    fruits and veggies

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
 /* setup your angular app here */
+var app = angular.module('FruitsnVeggies', []);
+app.controller('fruits-n-veggies', ['$scope', function($scope){
+	console.log("controller loaded");
+
+var fruits = [
+  'Apple',
+  'Apricot',
+  'Banana'
+];
+
+var vegetables = [
+  'Artichoke',
+  'Arugula',
+  'Asparagus'
+];
+
+  $scope.newTodo = "";
+  $scope.todo = [
+  'Apple',
+  'Apricot',
+  'Banana',
+  'Artichoke',
+  'Arugula',
+  'Asparagus'
+  ];
+
+  $scope.itemPosition = undefined;
+
+  $scope.moveLeft = function(move) {
+  	// $scope.itemLeft = $scope.todo;
+  	$scope.itemPosition = "left";
+  }
+
+  $scope.moveRight = function(move) {
+  	$scope.itemRight = $scope.todo;
+  }
+
+
+
+
+
+
+}]);
 
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
